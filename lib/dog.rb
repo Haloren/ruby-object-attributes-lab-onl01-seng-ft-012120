08df944d63ede4a3b9689be2db4dfc6824a99f6e
Kyle McKenna <mckennakyler@gmail.com>
2020-01-28 17:51:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 class Dog 
-  def initialize(name)
-    @name = name
+  def name=(dog_name)
+    @name = dog_name
   end
-  # def name
-  #   @name 
-  # end 
+  def name
+    @name 
+  end 
   
-  # def name=(new_name) 
-  #   @name = new_name 
-  # end   
-  
-  def initialize(breed) 
+  def breed= (breed) 
     @breed = breed 
   end
   # def breed 
